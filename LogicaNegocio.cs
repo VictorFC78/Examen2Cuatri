5ba9ae026661d072500818d9af3b0fd3e02d94bd
VictorFC78 <zpy57720@educastur.es>
2024-05-26 13:21:59 +0200

    Agregar archivos de proyecto.

---
+++ LogicaNegocio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen5BPJ
+{
+    public static class LogicaNegocio
+    {
+
+        /// <summary>
+        /// Obtiene una lista completa de contenedores
+        /// </summary>
+        /// <returns>Devuelve una lista con todos los contenedores</returns>
+        private static ObservableCollection<Contenedor> retValue;
+
+        public static ObservableCollection<Contenedor> RetValue { get => retValue; set => retValue = value; }
+
+        public static ObservableCollection<Contenedor> GetAllContenedores()
+        {
+            RetValue = new ObservableCollection<Contenedor>();
+            RetValue.Add(new Contenedor("D0976", 72, 53, "IND"));
+            RetValue.Add(new Contenedor("X9769", 52, 67, "IND"));
+            RetValue.Add(new Contenedor("P0966", 74, 75, "FRA"));
+            RetValue.Add(new Contenedor("D0980", 51, 15, "LIQ"));
+            RetValue.Add(new Contenedor("F76435", 56, 27, "SOL"));
+            RetValue.Add(new Contenedor("L7272", 77, 35, "FRA"));
+            RetValue.Add(new Contenedor("K0962", 153, 46, "LIQ"));
+            RetValue.Add(new Contenedor("J1021", 12, 46, "SOL"));
+            RetValue.Add(new Contenedor("A4501", 24, 65, "IND"));
+            return RetValue;
+        }
+        public static ObservableCollection<Contenedor> GetContenedoresTipoContenido(String  tipo) 
+        { 
+            ObservableCollection<Contenedor> lista= new ObservableCollection<Contenedor>();
+            foreach(Contenedor c in RetValue)
+            {
+                if (c.TipoContenido.Equals(tipo)) lista.Add(c);
+            }
+            return lista;
+        }
+        public static double pesomedio(ObservableCollection<Contenedor> lista)
+        {
+            double pesoMedio = 0;
+            foreach(Contenedor c in lista)
+            {
+                pesoMedio = pesoMedio + c.Peso;
+            }
+            return pesoMedio/lista.Count;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Examen5BPJ;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examen2Cuatri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Contenedor> contenedores;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contenedores=LogicaNegocio.GetAllContenedores();
+            btn.IsEnabled = false;
+            this.DataContext = contenedores;
+
+        }
+
+        private void validar(object sender, TextChangedEventArgs e)
+        {
+            ObservableCollection<Contenedor> lista = LogicaNegocio.GetAllContenedores();
+            Boolean correcto = false;
+            for(int i = 0; i < lista.Count && correcto == false; i++)
+            {
+                if (lista[i].TipoContenido.Equals(txt.Text.ToString()))
+                {
+                    correcto = true;
+                    btn.IsEnabled = true;
+                }
+                else
+                {
+                    btn.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            contenedores=LogicaNegocio.GetContenedoresTipoContenido(txt.Text);
+            this.DataContext = contenedores;
+            double pesoMedio=LogicaNegocio.pesomedio(contenedores);
+            pm.Text = pesoMedio.ToString();
+        }
+    }
+}
